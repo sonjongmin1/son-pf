38f759cbff842e4daac90788c3a11f3eed4a06b4
jong_min <whdals0525@gmail.com>
2025-04-17 22:16:41 +0900

    add

--- js/contact.js
+++ js/contact.js
@@ -5,7 +5,7 @@ window.onload = function () {
     .timeline({
       scrollTrigger: {
         trigger: '.contact-wrap',
-        start: '30% 55%',
+        start: '20% 55%',
         end: '100% 60%',
         scrub: 1,
         // markers: true,
@@ -13,84 +13,58 @@ window.onload = function () {
     })
     .to(
       '.contact-logoWrap #t',
-      { x: '0', y: '1500', rotate: '0', ease: 'none', duration: 3 },
+      { x: '0', y: '1000', rotate: '0', ease: 'none', duration: 3 },
       0.3
     )
     .to(
       '.contact-logoWrap #h',
-      { x: '0', y: '1400', rotate: '0', ease: 'none', duration: 3 },
+      { x: '0', y: '900', rotate: '0', ease: 'none', duration: 3 },
       0.3
     )
     .to(
       '.contact-logoWrap #a',
-      { x: '0', y: '1500', rotate: '0', ease: 'none', duration: 3 },
+      { x: '0', y: '1000', rotate: '0', ease: 'none', duration: 3 },
       0.3
     )
     .to(
       '.contact-logoWrap #n',
-      { x: '0', y: '1400', rotate: '0', ease: 'none', duration: 3 },
+      { x: '0', y: '900', rotate: '0', ease: 'none', duration: 3 },
       0.3
     )
     .to(
       '.contact-logoWrap #k',
-      { x: '0', y: '1500', rotate: '0', ease: 'none', duration: 3 },
+      { x: '0', y: '1000', rotate: '0', ease: 'none', duration: 3 },
       0.3
     )
     .to(
       '.contact-logoWrap #s',
-      { x: '0', y: '1400', rotate: '0', ease: 'none', duration: 3 },
+      { x: '0', y: '900', rotate: '0', ease: 'none', duration: 3 },
       0.3
     )
     .to(
       '.contact-logoWrap #f',
-      { x: '0', y: '1500', rotate: '0', ease: 'none', duration: 3 },
+      { x: '0', y: '1000', rotate: '0', ease: 'none', duration: 3 },
       0.3
     )
     .to(
       '.contact-logoWrap #o',
-      { x: '0', y: '1400', rotate: '0', ease: 'none', duration: 3 },
+      { x: '0', y: '900', rotate: '0', ease: 'none', duration: 3 },
       0.3
     )
     .to(
       '.contact-logoWrap #r',
-      { x: '0', y: '1500', rotate: '0', ease: 'none', duration: 3 },
+      { x: '0', y: '1000', rotate: '0', ease: 'none', duration: 3 },
       0.3
     );
+  gsap.to('.contact-wrap', {
+    backgroundColor: '#1c2b3c', // 원하는 색상
+    scrollTrigger: {
+      trigger: '.contact-wrap',
+      start: '0% 100%',
+      end: '20% 15%',
+      scrub: 1,
+      markers: true,
+    },
+  });
+  // .text-wave 텍스트 색상 변경
 };
-
-$(function () {
-  gsap
-    .timeline({
-      scrollTrigger: {
-        trigger: '.contact-intro',
-        start: 'top 75%',
-        end: '50% 25%',
-        scrub: 1,
-        // markers: true,
-      },
-    })
-    .fromTo(
-      '.contact-intro .a',
-      { x: '-100%' },
-      { x: '0%', ease: 'none', duration: 5, color: '#fff' },
-      0
-    )
-    .fromTo(
-      '.contact-intro .b',
-      { x: '100%' },
-      { x: '0%', ease: 'none', duration: 5, color: '#fff' },
-      0
-    )
-    // .fromTo(
-    //   '.contact-intro .c',
-    //   { x: '-100%' },
-    //   { x: '0%', ease: 'none', duration: 5, color: 'orange' },
-    //   0
-    // )
-    .to('.contact-intro', {
-      backgroundColor: '#1c2b3c',
-      ease: 'none',
-      duration: 3,
-    }),
-    2;
-});
