226c1486a217dcc839403dfff9f0c50ffc5af814
jong_min <whdals0525@gmail.com>
2025-04-21 21:11:30 +0900

    add

--- js/text-banner.js
+++ js/text-banner.js
@@ -1,17 +1,37 @@
 // simplyScroll
-$(function () {
-  $('.text-banner .list-hash').simplyScroll({
-    speed: 4,
-    pauseOnHover: false,
-    pauseOnTouch: false,
+if (window.matchMedia('(min-width: 769px)').matches) {
+  $(function () {
+    $('.text-banner .list-hash').simplyScroll({
+      speed: 4,
+      pauseOnHover: false,
+      pauseOnTouch: false,
+    });
   });
-});
 
-$(function () {
-  $('.text-banner .list-en').simplyScroll({
-    speed: 4,
-    pauseOnHover: false,
-    pauseOnTouch: false,
-    direction: 'backwards',
+  $(function () {
+    $('.text-banner .list-en').simplyScroll({
+      speed: 4,
+      pauseOnHover: false,
+      pauseOnTouch: false,
+      direction: 'backwards',
+    });
   });
-});
+}
+if (window.matchMedia('(max-width: 768px)').matches) {
+  $(function () {
+    $('.text-banner .list-hash').simplyScroll({
+      speed: 1,
+      pauseOnHover: false,
+      pauseOnTouch: false,
+    });
+  });
+
+  $(function () {
+    $('.text-banner .list-en').simplyScroll({
+      speed: 1,
+      pauseOnHover: false,
+      pauseOnTouch: false,
+      direction: 'backwards',
+    });
+  });
+}
