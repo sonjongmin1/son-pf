f3ed365faef9199db90b2d46728e9603cca14aed
jong_min <whdals0525@gmail.com>
2025-04-21 22:24:21 +0900

    add

--- js/footer.js
+++ js/footer.js
@@ -1,15 +1,33 @@
 gsap.registerPlugin(ScrollTrigger);
 
-$(function () {
-  gsap.to('.z-con01', {
-    scrollTrigger: {
-      trigger: '.text-wave',
-      start: '100% 50%',
-      end: '100% 50%',
-      scrub: 1,
-      // markers: true,
-    },
-    y: -200,
-    duration: 5,
+if (window.matchMedia('(min-width: 769px)').matches) {
+  $(function () {
+    gsap.to('.z-con01', {
+      scrollTrigger: {
+        trigger: '.text-wave',
+        start: '100% 50%',
+        end: '100% 50%',
+        scrub: 1,
+        // markers: true,
+      },
+      y: -200,
+      duration: 5,
+    });
   });
-});
+}
+
+if (window.matchMedia('(max-width: 768px)').matches) {
+  $(function () {
+    gsap.to('.z-con01', {
+      scrollTrigger: {
+        trigger: '.text-wave',
+        start: '100% 100%',
+        end: '100% 50%',
+        scrub: 1,
+        // markers: true,
+      },
+      y: -160,
+      duration: 5,
+    });
+  });
+}
